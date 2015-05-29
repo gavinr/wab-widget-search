4c86362cf285548b5c6221bdc16c8153fa9338f1
Gavin Rehkemper <gavreh@gmail.com>
2015-05-29 11:55:35 -0500

    updating widgets list

--- src/app/widget-locations.js
+++ src/app/widget-locations.js
@@ -7,13 +7,10 @@ define([
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/BatchEditor",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/BatchEditor/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/BombThreat",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/BombThreat/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/CI_KR_Chart",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/CI_KR_Chart/manifest.json"},
-		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/CostAnalysis",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/CostAnalysis/manifest.json"},
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/CostAnalysis/CostAnalysis",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/CostAnalysis/CostAnalysis/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ERG",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ERG/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/GeoLookup",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/GeoLookup/manifest.json"},
-		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ImageInfo",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ImageInfo/manifest.json"},
-		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ImageMensuration",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ImageMensuration/manifest.json"},
-		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ImageProperties",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ImageProperties/manifest.json"},
-		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ImageSelect",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ImageSelect/manifest.json"},
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ImageDiscovery",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ImageDiscovery/manifest.json"},
 		{url:"https://github.com/Esri/arcgis-webappbuilder-widgets-themes/tree/master/widgets/Bookmark",manifestUrl:"//rawgit.com/Esri/arcgis-webappbuilder-widgets-themes/master/widgets/Bookmark/manifest.json"},
 		{url:"https://github.com/Esri/arcgis-webappbuilder-widgets-themes/tree/master/widgets/GeoChat",manifestUrl:"//rawgit.com/Esri/arcgis-webappbuilder-widgets-themes/master/widgets/GeoChat/manifest.json"},
 		{url:"https://github.com/gavinr/wab-wikipedia-search",manifestUrl:"//rawgit.com/gavinr/wab-wikipedia-search/master/manifest.json"},
@@ -24,5 +21,7 @@ define([
 		{url:"https://github.com/magis-nc/esri-webappbuilder-widget-eDraw",manifestUrl:"//rawgit.com/magis-nc/esri-webappbuilder-widget-eDraw/master/manifest.json"},
 		{url:"https://github.com/mlewis22/WebAppBuilder/tree/master/Google%20Analytics/Widget/GoogleAnalytics",manifestUrl:"//rawgit.com/mlewis22/WebAppBuilder/master/Google%20Analytics/Widget/GoogleAnalytics/manifest.json"},
 		{url:"https://github.com/voyagersearch/voyager-dojo-widget/tree/master/voyager-web-appbuilder",manifestUrl:"//rawgit.com/voyagersearch/voyager-dojo-widget/master/voyager-web-appbuilder/Voyager/manifest.json"}
+
+		// {url:"",manifestUrl:""}, // TODO widgets in https://github.com/ArcGIS/WAB-Image-Services-Widgets/tree/master/imagery_widgets
 	];
 });
