e33c55a07d74c9291e23268a4c9d488a34d0ffd6
Gavin Rehkemper <gavreh@gmail.com>
2015-07-20 22:11:54 -0700

    adding esri solutions team widgets

--- src/app/widget-locations.js
+++ src/app/widget-locations.js
@@ -4,12 +4,20 @@ define([
 	declare
 ) {
 	return [
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/BatchAttributeEditor",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/BatchAttributeEditor/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/BombThreat",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/BombThreat/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/CI_KR_Chart",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/CI_KR_Chart/manifest.json"},
 		// {url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/CostAnalysis/CostAnalysis",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/CostAnalysis/CostAnalysis/manifest.json"},
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/DirectionsWithBarriers",manifestUrl:"https://rawgit.com/Esri/solutions-webappbuilder-widgets/master/DirectionsWithBarriers/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ERG",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ERG/manifest.json"},
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ElevationProfileTable",manifestUrl:"https://rawgit.com/Esri/solutions-webappbuilder-widgets/master/ElevationProfileTable/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/GeoLookup",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/GeoLookup/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ImageDiscovery",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ImageDiscovery/manifest.json"},
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/MissionEditor",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/MissionEditor/manifest.json"},
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/NetworkTrace",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/NetworkTrace/manifest.json"},
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ServiceFeasibility",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ServiceFeasibility/manifest.json"},
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/Swipe",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/Swipe/manifest.json"},
+
 		{url:"https://github.com/Esri/arcgis-webappbuilder-widgets-themes/tree/master/widgets/Bookmark",manifestUrl:"//rawgit.com/Esri/arcgis-webappbuilder-widgets-themes/master/widgets/Bookmark/manifest.json"},
 		{url:"https://github.com/Esri/arcgis-webappbuilder-widgets-themes/tree/master/widgets/GeoChat",manifestUrl:"//rawgit.com/Esri/arcgis-webappbuilder-widgets-themes/master/widgets/GeoChat/manifest.json"},
 		{url:"https://github.com/gavinr/wab-wikipedia-search",manifestUrl:"//rawgit.com/gavinr/wab-wikipedia-search/master/manifest.json"},
