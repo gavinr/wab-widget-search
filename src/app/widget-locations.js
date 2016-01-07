afe461065385ee7f99f45ec35f7abbd3f0bb8496
Gavin Rehkemper <gavreh@gmail.com>
2016-01-06 23:34:37 -0600

    additional widgets.

--- src/app/widget-locations.js
+++ src/app/widget-locations.js
@@ -5,7 +5,6 @@ define([
 ) {
 	// helpful: https://github.com/search?q=%22wabVersion%22&type=Code&utf8=%E2%9C%93
 	return [
-		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/BatchAttributeEditor",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/BatchAttributeEditor/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/BombThreat",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/BombThreat/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/CI_KR_Chart",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/CI_KR_Chart/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ERG",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ERG/manifest.json"},
@@ -13,6 +12,7 @@ define([
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ImageDiscovery",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ImageDiscovery/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/NetworkTrace",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/NetworkTrace/manifest.json"},
 		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/ServiceFeasibility",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/ServiceFeasibility/manifest.json"},
+		{url:"https://github.com/Esri/solutions-webappbuilder-widgets/tree/master/FilterEditor",manifestUrl:"//rawgit.com/Esri/solutions-webappbuilder-widgets/master/FilterEditor/manifest.json"},
 		{url:"https://github.com/Esri/arcgis-webappbuilder-widgets-themes/tree/master/widgets/Bookmark",manifestUrl:"//rawgit.com/Esri/arcgis-webappbuilder-widgets-themes/master/widgets/Bookmark/manifest.json"},
 		{url:"https://github.com/Esri/arcgis-webappbuilder-widgets-themes/tree/master/widgets/GeoChat",manifestUrl:"//rawgit.com/Esri/arcgis-webappbuilder-widgets-themes/master/widgets/GeoChat/manifest.json"},
 		{url:"https://github.com/gavinr/wab-wikipedia-search",manifestUrl:"//rawgit.com/gavinr/wab-wikipedia-search/master/manifest.json"},
@@ -28,6 +28,7 @@ define([
 		{url:"https://github.com/USEPA/AddLayerFromWeb-widget-js/tree/master/AddLayerFromWeb",manifestUrl:"//rawgit.com/USEPA/AddLayerFromWeb-widget-js/master/AddLayerFromWeb/manifest.json"},
 		{url:"https://github.com/EsriJapan/arcgis-webappbuilder-widgets-themes/tree/master/widgets/Clustering",manifestUrl:"//rawgit.com/EsriJapan/arcgis-webappbuilder-widgets-themes/master/widgets/Clustering/manifest.json"},
 		{url:"https://github.com/EsriJapan/arcgis-webappbuilder-widgets-themes/tree/master/widgets/RandomPoints",manifestUrl:"//rawgit.com/EsriJapan/arcgis-webappbuilder-widgets-themes/master/widgets/RandomPoints/manifest.json"},
+		{url:"https://github.com/EsriJapan/arcgis-webappbuilder-widgets-themes/tree/master/widgets/StatusMonitoring",manifestUrl:"//rawgit.com/EsriJapan/arcgis-webappbuilder-widgets-themes/master/widgets/StatusMonitoring/manifest.json"},
 		{url:"https://github.com/gisfromscratch/webapp-builder-demos/tree/master/samples/Sample%20Widgets/WebAppWidgets/widgets/BubbleMessage",manifestUrl:"//rawgit.com/gisfromscratch/webapp-builder-demos/master/samples/Sample%20Widgets/WebAppWidgets/widgets/BubbleMessage/manifest.json"},
 		{url:"https://github.com/WestonSF/ArcGISWebAppBuilderLocateCoordinatesWidget",manifestUrl:"//rawgit.com/WestonSF/ArcGISWebAppBuilderLocateCoordinatesWidget/master/manifest.json"},
 		{url:"https://github.com/kevinsagis/ScreenshotDemo/tree/master/ScreenshotWidget/widgets/Screenshot",manifestUrl:"//rawgit.com/kevinsagis/ScreenshotDemo/master/ScreenshotWidget/widgets/Screenshot/manifest.json"},
@@ -35,7 +36,9 @@ define([
 		{url:"https://github.com/URSpatial/WAB_AddShapefile",manifestUrl:"//rawgit.com/URSpatial/WAB_AddShapefile/master/AddShapefile/manifest.json"},
 		{url:"https://github.com/kevinsagis/EnhancedQuery/tree/master/eQuery/widgets/Enhanced%20Query",manifestUrl:"//rawgit.com/kevinsagis/EnhancedQuery/master/eQuery/widgets/Enhanced%20Query/manifest.json"},
 		{url:"https://github.com/mdgio/WAB/tree/master/AddData",manifestUrl:"//rawgit.com/mdgio/WAB/master/AddData/manifest.json"},
-		{url:"https://github.com/cmndrbensisko/LayerListLite",manifestUrl:"//rawgit.com/cmndrbensisko/LayerListLite/master/manifest.json"}
+		{url:"https://github.com/cmndrbensisko/LayerListLite",manifestUrl:"//rawgit.com/cmndrbensisko/LayerListLite/master/manifest.json"},
+		{url:"https://github.com/jgrayson-apl/ISTimeSelect",manifestUrl:"//rawgit.com/jgrayson-apl/ISTimeSelect/master/manifest.json"},
+		{url:"https://github.com/markdeaton/closest-facility-wab-js",manifestUrl:"//rawgit.com/markdeaton/closest-facility-wab-js/master/manifest.json"},
 		// {url:"",manifestUrl:""},
 	];
 });
